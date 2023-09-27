67ca674d9b0cb26150978e090fb6941d3baafa1e
Erley Barbosa da Silva <56356543+erleybru@users.noreply.github.com>
2023-09-27 19:17:55 -0300

    Create Chickens01.java

---
+++ src/br/com/senaisp/exemplos/Chickens01.java
@@ -0,0 +1,23 @@
+package br.com.senaisp.erley.secao32;
+
+public class Chickens01 {
+
+	public static void main(String[] args) {
+		// aqui é a quantidade de galinhas
+		int chickenCount = 0;
+		// aqui é a quantidade de ovos que cada galinha bota por dia
+		int eggsPerChicken = 0;
+		// acumula a quantidade de ovos
+		int totalOvos = 0;
+		
+		// Na segunda-feira
+		
+		// terça-feira
+		
+		// Quarta-feira
+		
+		// exibir a quantidade de ovos coletados até a quarta-feira
+		System.out.println("Quantidade de ovos: " + totalOvos);
+	}
+
+}
