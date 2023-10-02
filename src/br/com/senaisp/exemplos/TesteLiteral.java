e93c1391051b6a99b12f3c4eda3b33f80a06f2d3
Erley Barbosa da Silva <56356543+erleybru@users.noreply.github.com>
2023-10-02 19:47:09 -0300

    Create TesteLiteral.java

---
+++ src/br/com/senaisp/exemplos/TesteLiteral.java
@@ -0,0 +1,25 @@
+package br.com.senaisp.erley.literais;
+
+public class TesteLiteral {
+
+	public static void main(String[] args) {
+		// criando 5 variaveis tipo char
+		char letra1, letra2, letra3, letra4, letra5;
+//		String texto = new String("SENAI"); //ou
+//		String texto2 = "SENAI";
+		
+		// inicializar as variaveis, ou seja colocar um
+		// valor inicial
+		letra1 = 'S';
+		letra2 = 'E';
+		letra3 = 'N';
+		letra4 = 'A';
+		letra5 = 'I';
+		// Exibir dados na tela
+		System.out.print("" + letra1 + letra2 + letra3 + letra4 +
+				 letra5);
+				 
+//		System.out.println(texto);
+		System.out.println();
+	}
+}
