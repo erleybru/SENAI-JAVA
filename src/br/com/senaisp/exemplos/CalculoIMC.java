0321411e80ba2b583859c52b73f83b495bee3a12
Erley Barbosa da Silva <56356543+erleybru@users.noreply.github.com>
2023-10-16 21:29:39 -0300

    Create CalculoIMC.java

---
+++ src/br/com/senaisp/exemplos/CalculoIMC.java
@@ -0,0 +1,36 @@
+package br.com.senaisp.erley.secao45exerc4;
+
+import javax.swing.JOptionPane;
+
+public class CalculoIMC {
+
+	public static void main(String[] args) {
+		double peso;
+		double altura;
+		double imc;
+		
+		// solicitar o peso da pessoa
+		peso = Double.parseDouble(
+				JOptionPane.showInputDialog("Informe o peso"));
+		
+		altura = Double.parseDouble(
+				JOptionPane.showInputDialog("Informe a altura"));
+		
+		// calculo do IMC
+		imc = peso / Math.pow(altura, 2);
+		
+		// mostrar resultado
+		JOptionPane.showMessageDialog(null, "IMC = " + imc);
+		
+//		Entre 18,5 e 24,99	Peso normal
+		if(imc >= 18.5 && imc <= 24.99) {
+			JOptionPane.showMessageDialog(null, "Peso Normal");
+		}
+//		Entre 25 e 29,99	Acima do peso
+		if(imc >= 25 && imc <= 29,99) {
+			JOptionPane.showMessageDialog(null, "Acima do peso")
+		}
+		
+	} // final do método main
+
+} // final da classe
